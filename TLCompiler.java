a21a674924299a3af43a4a743e18c269bc4bc5a9
Xue Qin <xue@Xues-MacBook-Pro.local>
2017-02-07 19:50:25 -0600

    Update scanner and main java files

---
+++ TLCompiler.java
@@ -0,0 +1,17 @@
+package TLCompiler;
+
+/**
+*
+* @author Tuan Dinh, Xue Qin
+*/
+
+public class TLCompiler {
+
+	public static void main(String[] args) {
+		String base = "";
+		if (args[0]!= null && args[0].endsWith(".tl") || args[0].endsWith(".TL"))
+			base = args[0].substring(0, args[0].length() - 3);
+		Scanner.scan(base);
+	}
+
+}
